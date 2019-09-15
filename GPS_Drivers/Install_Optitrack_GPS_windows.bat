c7671b905d5215a024f53a2b602cf391d46e1408
Testingthi <tommie.albers@ziggo.nl>
2019-09-15 22:44:24 +0200

    Update windows install script

--- GPS_Drivers/Install_Optitrack_GPS_windows.bat
+++ GPS_Drivers/Install_Optitrack_GPS_windows.bat
@@ -12,4 +12,33 @@ REM ECHO %path%\src\drivers\gps\devices\src
 
 ECHO Copying optitrack driver files
 
+ECHO drv_gps.h
+copy "%CD%\drv_gps.h" "%path%\src\drivers"
+
+ECHO CMakeLists.txt
+copy "%CD%\gps_optitrack\CMakeLists.txt" "%path%\src\drivers\gps"
+
+ECHO gps.copyp
+copy "%CD%\gps_optitrack\gps.cpp" "%path%\src\drivers\gps"
+
+ECHO ashtech.cpp
+ECHO ashtech.h
+ECHO gps_helper.cpp
+ECHO gps_helper.h
+ECHO marv.cpp
+ECHO marv.h
+ECHO mtk.cpp
+ECHO mtk.h
+ECHO optitrack.cpp
+ECHO optitrack.h
+ECHO ubx.cpp
+ECHO ubx.h
+copy "%CD%\gps_optitrack\devices\src\*.*" "%path%\src\drivers\gps\devices"
+
+ECHO vehicle_gps_optitrack.msg
+copy "%CD%\gps_optitrack\msg\vehicle_gps_optitrack.msg" "%path%\msg"
+
+ECHO CMakeLists.txt
+copy "%CD%\gps_optitrack\msg\CMakeLists.txt" "%path%\msg"
+
 PAUSE
